7d230cfe887830d2f6b6ad5624f6f5c03608f959
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-28 09:58:43 +0100

    Add project files.

---
+++ Open-Lab-2.6/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_2._6
+{
+    class Calculator
+    {
+        public bool DivisibleByHundred(int number)
+        {
+            if (number % 100 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Open-Lab-2.6/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Open_Lab_2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator number = new Calculator();
+            Console.WriteLine(number.DivisibleByHundred(3200));
+        }
+    }
+}
